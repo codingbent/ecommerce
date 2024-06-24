decec86f3143b23c9193e4ad986605c9d3401ac4
codingbent <121689258+codingbent@users.noreply.github.com>
2024-06-24 17:28:49 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name="ecommerce";
+
+// Create connection
+$con =mysqli_connect($servername, $username, $password, $db_name);
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo("Connection failed: " .mysqli_connect_errno() );
+}
+?>
