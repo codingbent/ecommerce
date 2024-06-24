6df358d5de9ac84777a6e0cb8cc948b3393cabb6
codingbent <121689258+codingbent@users.noreply.github.com>
2024-06-24 19:54:28 +0530

    layout.php

--- layout.php
+++ layout.php
@@ -1,6 +1,7 @@
 <?php
 
 include '../connection.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -9,25 +10,37 @@ include '../connection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-       
-   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../main.css">
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>Bootstrap Example</title>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
   </head>
 </head>
 <body>
 <?php
 include 'adminNav.php';
 ?>
-<main>
-
-
-</main>
-
+<p class="fs-3 text m-3">DashBoard</p>
+<div class="container px-4 text-center">
+  <div class="row gx-5">
+    <div class="col">
+     <div class="p-3">
+      <p>Total Sales</p>
+      <p>rhb</p>
+     </div>
+    </div>
+    <div class="col">
+      <div class="p-3">
+        <p>Total Sales</p>
+        <p>rhb</p></div>
+    </div>
+    <div class="col">
+     <div class="p-3">
+      <p>Total Sales</p>
+      <p>rhb</p>
+    </div>
+    </div>
+  </div>
+</div>
 <?php
 include 'footer.php';
 ?>
