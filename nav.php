ad97eda85c62b5d8830357e017f73e17cf61de38
codingbent <121689258+codingbent@users.noreply.github.com>
2024-06-25 17:39:53 +0530

    nav.php

--- nav.php
+++ nav.php
@@ -1,6 +1,16 @@
 <?php
 session_start();
 include 'connection.php';
+
+// Count the total rows in the cart
+$sqlCountRows = "SELECT COUNT(*) AS total_rows FROM cart";
+$resultCountRows = $con->query($sqlCountRows);
+if ($resultCountRows) {
+    $rowTotalRows = $resultCountRows->fetch_assoc();
+    $totalRows = $rowTotalRows['total_rows'];
+} else {
+    $totalRows = 0;
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,10 +22,6 @@ include 'connection.php';
     <link rel="stylesheet" href="main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gybby3bZ6rmxa/j9FZpFYG3WEe7xU7arxM0cl6rMFF0CK3wC1T" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhG+LOtFfOFvGQt43go5fAwL2xEX5dE1l27VYjUq1R0Jzt+K3sdyV7vGzLPL" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="index.js"></script>
 </head>
 <body>
 <div class="container px-3 ">                           
@@ -39,8 +45,6 @@ include 'connection.php';
                               </span>
                           </a>
                           <?php
-                          
-
                           if (empty(@$_SESSION['email'])) {
                               // User is not logged in
                               echo '<a href="login.php">Login</a>/<a href="registration.php">Registration</a>';
@@ -48,7 +52,7 @@ include 'connection.php';
                               echo '
                              <div class="btn-group">
                               <button type="button" class="btn btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">'
-                              . htmlspecialchars(@$_SESSION['name'], ENT_QUOTES, 'UTF-8') . 
+                              . htmlspecialchars(@$_SESSION['name']) . 
                               '</button>
                               <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="user.php">My Profile</a></li>
@@ -58,10 +62,10 @@ include 'connection.php';
                             </div>';
                           }
                           ?>
-                          <a type="button" class="btn position-relative">
+                          <a type="button" class="btn position-relative" href="cart.php">
                             <i class="fa fa-shopping-cart"style="font-size:36px;"></i>
                             <span class="position-absolute translate-middle badge rounded-pill bg-success" style="top: 10px;left: 50px;">
-                                1
+                                <?php echo $totalRows ;?>
                                 <span class="visually-hidden">New Alerts</span>
                               </span>
                           </a>
@@ -121,14 +125,6 @@ include 'connection.php';
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">Separated link</a></li>
             </ul>
-            <button class="btn btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Account</button>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Separated link</a></li>
-            </ul>
             <button class="btn btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dashboard</button>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#">Action</a></li>
@@ -137,7 +133,16 @@ include 'connection.php';
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">Separated link</a></li>
             </ul>
+            <?php if(!empty($_SESSION['is_admin'])){?>
+            <button class="btn btn-lg dropdown-toggle" id="product" type="button" data-bs-toggle="dropdown" aria-expanded="false">Products</button>
+            <ul class="dropdown-menu" id="product">
+              <li><a class="dropdown-item" href="addProduct.php">Add Product</a></li>
+              <li><a class="dropdown-item" href="productList.php">Product list</a></li>
+              <li><a class="dropdown-item" href="#">Categories</a></li>
+            </ul>
             <button class="btn btn-lg" type="button">Docs</button>
+            <?php }?>
+
         </div>
             
     </div>
