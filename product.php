56df1e214d0f27d2d23f5ddf2ab2ca11c2947bb9
codingbent <121689258+codingbent@users.noreply.github.com>
2024-06-25 17:40:15 +0530

    product.php

--- product.php
+++ product.php
@@ -1,3 +1,6 @@
+<?php
+include 'connection.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,45 +12,88 @@
 </head>
 <body>
 <section class="four">
-        <div class="container text-center">
-            <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-                <?php
-                // Fetch product details from database
-                $query = "SELECT title, label, image, price FROM product";
-                $result = $con->query($query);
+    <div class="container text-center">
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+            <?php
+            // Fetch product details from database
+            $query = "SELECT p_id, title, label, image, price FROM product";
+            $result = $con->query($query);
 
-                // Check if there are products in the database
-                if ($result->num_rows > 0) {
-                    // Output data of each row
-                    while($row = $result->fetch_assoc()) {
-                        echo '<div class="col m-1">';
-                        echo '    <div class="p-3">';
-                        echo '        <div class="card m-1" ">';
-                        echo '            <img src="images/' . $row["image"] . '" class="card-img-top" alt="...">';
-                        echo '            <div class="card-body">';
-                        echo '                <h5 class="card-title">' . $row["title"] . '</h5>';
-                        echo '                <p class="card-text">' . $row["label"] . '</p>';
-                        echo '                <p class="card-text">₹' . $row["price"] . '</p>';
-                        echo '                <div class="d-flex justify-content-center">';
-                        echo '                <a href="#" class="btn btn-success">-</a>';
-                        echo '                <input type="text" id="totalProduct" class="w-50">';
-                        echo '                <a href="#" class="btn btn-success">+</a>';
-                        echo '                </div>';
-                        echo '                <button class="btn btn-success w-75 mt-2" type="button">Add to Cart</button>';
-                        echo '            </div>';
-                        echo '        </div>';
-                        echo '    </div>';
-                        echo '</div>';
-                    }
-                } else {
-                    echo '<p>No products found</p>';
+            // Check if there are products in the database
+            if ($result->num_rows > 0) {
+                // Output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo '<div class="col m-1">';
+                    echo '    <div class="p-3">';
+                    echo '        <div class="card m-1">';
+                    echo '            <img src="images/' . $row["image"] . '" class="card-img-top" alt="...">';
+                    echo '            <div class="card-body">';
+                    echo '                <h5 class="card-title">' . $row["title"] . '</h5>';
+                    echo '                <p class="card-text">' . $row["label"] . '</p>';
+                    echo '                <p class="card-text">₹' . $row["price"] . '</p>';
+                    echo '                <div class="d-flex justify-content-center">';
+                    echo '                <button class="btn btn-success" onclick="decrementQuantity(' . $row["p_id"] . ')">-</button>';
+                    echo '                <input type="text" id="productQuantity_' . $row["p_id"] . '" class="w-50 text-center" value="0">';
+                    echo '                <button class="btn btn-success" onclick="incrementQuantity(' . $row["p_id"] . ')">+</button>';
+                    echo '                </div>';
+                    echo '                <button class="btn btn-success w-75 mt-2" type="button">Add to Cart</button>';
+                    echo '            </div>';
+                    echo '        </div>';
+                    echo '    </div>';
+                    echo '</div>';
                 }
+            } else {
+                echo '<p>No products found</p>';
+            }
 
-                // Close the connection
-                $con->close();
-                ?>
-            </div>
+            // Close the connection
+            $con->close();
+            ?>
         </div>
-      </section>
+    </div>
+</section>
+<?php
+ include 'admin/footer.php'
+ ?>
+
 </body>
+<script>
+    function incrementQuantity(productId) {
+        var quantityInput = document.getElementById('productQuantity_' + productId);
+        var quantity = parseInt(quantityInput.value);
+        quantity++;
+        quantityInput.value = quantity;
+    }
+
+    function decrementQuantity(productId) {
+        var quantityInput = document.getElementById('productQuantity_' + productId);
+        var quantity = parseInt(quantityInput.value);
+        if (quantity > 0) {
+            quantity--;
+            quantityInput.value = quantity;
+        }
+    }
+
+    // function addToCart(productId) {
+    //     var quantityInput = document.getElementById('productQuantity_' + productId);
+    //     var quantity = parseInt(quantityInput.value);
+    //     if (quantity > 0) {
+    //         var xhr = new XMLHttpRequest();
+    //         xhr.open('POST', 'addToCart.php', true);
+    //         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    //         xhr.onload = function() {
+    //             if (xhr.status == 200) {
+    //                 alert('Product added to cart successfully!');
+    //             } else {
+    //                 alert('Error adding product to cart.');
+    //             }
+    //         };
+    //         xhr.send('productId=' + productId + '&quantity=' + quantity);
+    //     } else {
+    //         alert('Please select a quantity greater than 0.');
+    //     }
+    // }
+
+    // onclick="addToCart(' . $row["p_id"] . ')
+</script>
 </html>
