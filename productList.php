4e9f2f73f58529265822b4759d79ef53bc032d5b
codingbent <121689258+codingbent@users.noreply.github.com>
2024-06-25 17:41:56 +0530

    productList.php

--- productList.php
+++ productList.php
@@ -1,6 +1,29 @@
 <?php
-include 'adminNav.php';
+include 'nav.php';
 ?>
+<script>
+function remove(title){
+    if(confirm("Do you want to remove the product "+title+"?")){
+        <?php 
+        $title=title;
+        $sql="SELECT p_id from product where title="$title"";
+        $result=$con->query($sql);
+        ?>
+        alert('product removed');
+    }
+}
+</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
     <div class="mx-5">
         <div class="title d-flex justify-content-between ">
             <p class="fs-3 text">Product Grid</p>
@@ -21,13 +44,13 @@ include 'adminNav.php';
                         echo '<div class="col m-1">';
                         echo '    <div class="p-3">';
                         echo '        <div class="card m-1" ">';
-                        echo '            <img src="../images/' . $row["image"] . '" class="card-img-top" alt="...">';
+                        echo '            <img src="images/' . $row["image"] . '" class="card-img-top" alt="...">';
                         echo '            <div class="card-body">';
                         echo '                <h5 class="card-title">' . $row["title"] . '</h5>';
                         echo '                <p class="card-text">' . $row["label"] . '</p>';
                         echo '                <p class="card-text">₹' . $row["price"] . '</p>';
                         echo '                <a href="productEdit.php" class="btn btn-primary">Edit</a>';
-                        echo '                <a href="#" class="btn btn-primary">Delete</a>';
+                        echo '                <a onclick="remove(\'' . $row["title"] . '\')" class="btn btn-primary">Delete</a>';
                         echo '            </div>';
                         echo '        </div>';
                         echo '    </div>';
@@ -43,8 +66,8 @@ include 'adminNav.php';
             </div>
         </div>
     </div>
-
-
+            </body>
+            </html>
 <?php
 include 'footer.php';
 ?>
