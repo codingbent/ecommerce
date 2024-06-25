62324981b83b6963d4fa1ff423c563d78679199d
codingbent <121689258+codingbent@users.noreply.github.com>
2024-06-25 17:37:00 +0530

    adminNav.php

--- adminNav.php
+++ adminNav.php
@@ -1,8 +1,8 @@
-<?php 
-session_start();
-include '../connection.php';
-?>
-
+<!-- <?php 
+// session_start();
+// include '../connection.php';
+?> -->
+<!-- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,7 +24,10 @@ include '../connection.php';
         }
     </style>
 </head>
-<body>
+<body> -->
+<link rel="stylesheet" href="main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <nav class="navbar navbar-light fixed-top">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -58,8 +61,31 @@ include '../connection.php';
               <li><a class="dropdown-item" href="#">Brands</a></li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Orders
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Customer
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="customer.php">Customer</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -74,14 +100,17 @@ include '../connection.php';
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../login.php">Logout</a>
+          </li>
         </ul>
       </div>
     </div>
   </div>
 </nav>
 
-</body>
+<!-- </body>
 <?php 
-include 'footer.php';
-?>
-</html>
+// include 'footer.php';
+?> 
+</html> -->
