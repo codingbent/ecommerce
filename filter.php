15c89f69ff20fe71573f8deecafa64f2ca8a52a7
Abhed Agarwal <Abhed.Agarwal@rmsi.com>
2024-07-16 16:36:48 +0530

    edit

--- filter.php
+++ filter.php
@@ -37,7 +37,7 @@ $stmt = $con->prepare($sql);
 
 // Bind parameters dynamically
 if (!empty($parameters)) {
-    $types = str_repeat('s', count($parameters)); // Assuming all parameters are strings
+    $types = str_repeat('s', count($parameters)); 
     $stmt->bind_param($types, ...$parameters);
 }
 
@@ -81,4 +81,4 @@ $con->close();
   });
   console.log(p_id);
 }
-    </script>
+</script>
